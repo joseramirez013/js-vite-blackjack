c4636824f63adc2fdcb6bed6f9d67f1130dc8201
Juan Ramirez <jjramirez013@gmail.com>
2023-08-29 06:30:11 -0600

    First commit

---
+++ src/blackjack/usecases/index.js
@@ -0,0 +1,6 @@
+// Este archivo es conocido como Archivo Barril
+export { crearCartaHTML } from './crear-carta-html';
+export { crearDeck } from './crear-deck';
+export { pedirCarta } from './pedir-carta';
+export { turnoComputadora } from './turno-computadora';
+export { valorCarta } from './valor-carta';
